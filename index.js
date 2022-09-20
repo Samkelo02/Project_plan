afff7c46e4a2298db94f67a6b1e33d7d2865e7fe
Sibiya <sibiyasn02@gmail.com>
2022-09-20 16:30:36 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,41 @@
+import * as sqlite from 'sqlite';
+import sqlite3 from 'sqlite3';
+import express from 'express';
+
+const app = express();
+
+app.use(express.static('public'));
+app.use(express.json());
+
+const  db = await  sqlite.open({
+    filename:  './data_plan.db',
+    driver:  sqlite3.Database
+});
+
+console.log('db initialized');
+
+await db.exec(`PRAGMA foreign_keys = ON;`);
+
+await db.migrate();
+
+app.get('/api/company', async function(req, res) {
+    const company = await db.all(`select * from company`);
+    res.json({
+        company
+    });
+});
+
+
+
+console.log('done');
+
+    const PORT = process.env.PORT || 4003;
+    app.listen(PORT, function() {
+        console.log(`app started on $({PORT}`)
+
+    });
+
+
+
+
+
---
+++ migrations/002-create-customer-table.sql
@@ -0,0 +1,4 @@
+insert into customer (id, name, location, load) 
+    values (1, "Chris", "Auckland Park", 200); 
+           (2, "Jane", "Melville", 178);
+           (3, "Francis", "Rosebank", 160);
